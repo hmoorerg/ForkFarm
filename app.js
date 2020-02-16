af07955f4b201001bf67cc492454052fc8f46998
Henry Moore <hmoorerg@gmail.com>
2020-02-15 16:07:24 -0800

    Fixed halting bug

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 
 
 let key = process.env.TOM_TOM_KEY;
+
 coordinates = [
     { lat:37.792594, lon:-122.394724},
     { lat:37.790576, lon:-122.405324},
@@ -13,28 +14,29 @@ coordinates = [
 
 function convertCoordinatesToTomTom(coordinates){
     var coordinateString = "";
-    for (i = 0; i < coordinates.length; i++){
+    for (var i = 0; i < coordinates.length; i++){
         //Encode coordinate record
         coordinateString += coordinates[i].lat + "," + coordinates[i].lon;
 
         //Add colon between coordinate records
         if (i != coordinates.length-1){
-            coordinates+=":";
+            coordinateString+=":";
         }
     }
+
+    return coordinateString;
 }
 
 
 console.log("Making a request to TomTom");
 let urlCoordinates = convertCoordinatesToTomTom(coordinates);
-axios.get("https://api.tomtom.com/routing/1/calculateRoute/52.50931%2C13.42936%3A52.50274%2C13.43872/xml?avoid=unpavedRoads&key="+key)
+axios.get(`https://api.tomtom.com/routing/1/calculateRoute/${urlCoordinates}/json?instructionsType=text&computeBestOrder=true&avoid=unpavedRoads&travelMode=car&key=`+key)
 .then(response => {
-    console.log(response.data.url);
-    console.log(response.data.explanation);
+    console.log(response.data);
 
 })  
 .catch(error => {
     console.log(error);
-});
+  });
 
 
